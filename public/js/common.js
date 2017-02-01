62043e91b4d3cf90f31bdbc8b948588b8ddfed0f
Adam Svetec <adamsvetec@gmail.com>
2017-02-01 18:23:16 -0500

    add initial filtering capabilities for map

--- public/js/common.js
+++ public/js/common.js
@@ -4,58 +4,85 @@ var bathroom_cnt = 1;
 var rent_high = 700;
 var rent_low = 0;
 var year = "2016-2017";
-var sendData;
-function updateData(){
-    sendData = {"bedroom_cnt":bedroom_cnt, "bathroom_cnt":bathroom_cnt, "rent_high":rent_high, "rent_low":rent_low, "year":year};
 
-    $.get( '/filter', sendData, function(data) { alert("Success!") });
-    
+var map;
+var markers = [];
+
+function updateData(){
+	for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+    }
+    markers = [];
+	var filter = {"bedroom_cnt":bedroom_cnt, "bathroom_cnt":bathroom_cnt, "rent_high":rent_high, "rent_low":rent_low, "year":year};
+	$.get( '/filter', filter, function(rows) { 
+		for(i=0; i < rows.length; i++){
+			var location = {lat:rows[i].lat,lng:rows[i].lng};
+			addMarker(location);
+		}
+	});
+}
+
+function addMarker(location) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        markers.push(marker);
+      }
+
+function initMap() {
+	var lehigh_loc = {lat: 40.6069, lng: -75.3783};
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: lehigh_loc
+	});
+	updateData();
 }
 
 function clearFilter(){
-    alert("Someone needs to code this...")
-	}
+	alert("Someone needs to code this...")
+}
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById("myDropdown").classList.toggle("show");
 }
 
 function dropDown2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
+	document.getElementById("myDropdown2").classList.toggle("show");
 }
 
 function myFunctionBaths() {
-    document.getElementById("myDropdown3").classList.toggle("show");
+	document.getElementById("myDropdown3").classList.toggle("show");
 }
 
 function myFunctionYear() {
-    document.getElementById("myDropdown4").classList.toggle("show");
+	document.getElementById("myDropdown4").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+	if (!event.target.matches('.dropbtn')) {
 
-	var dropdowns = document.getElementsByClassName("dropdown-content");
-	var i;
-	for (i = 0; i < dropdowns.length; i++) {
-	    var openDropdown = dropdowns[i];
-	    if (openDropdown.classList.contains('show')) {
-		openDropdown.classList.remove('show');
-	    }
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
 	}
-    }
 }
 
-    function alertFunc1(){
+function alertFunc1(){
 	if($(map).is(":visible")){
-	    $(map).hide();
-	    $(list).show();
-	    mapStatus = 'list';
+		$(map).hide();
+		$(list).show();
+		mapStatus = 'list';
 	} else {
-	    $(list).hide();
-	    $(map).show();
-	    mapStatus = 'map';
-	    
+		$(list).hide();
+		$(map).show();
+		mapStatus = 'map';
+
 	}
-    }
+}
