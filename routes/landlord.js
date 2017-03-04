663043700c72e10019ba057d4e91d272af19f554
Adam Svetec <adamsvetec@gmail.com>
2017-03-04 09:11:44 -0500

    switch landlord routing and view to mongo

--- routes/landlord.js
+++ routes/landlord.js
@@ -1,32 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var winston = require('winston');
+var Landlord = require('../models/landlord');
+var House = require('../models/house');
 
 /* GET home page. */
-router.get('/landlord', function(req, res, next) {
-	console.log("in routing");
-  global.db.query('SELECT name, phone_num, email, website, AVG(leniency_rating) AS leniency_rating, AVG(fairness_rating) AS fairness_rating, AVG(repair_rating) AS repair_rating \
-    	FROM landlord, review_landlord \
-    	WHERE landlord.id=review_landlord.landlord_id \
-      AND landlord.id=? \
-      GROUP BY landlord.id; \
-      SELECT address, bedroom_cnt, bathroom_cnt, parking_spot_cnt, AVG(room_size_rating) AS room_size_rating, AVG(cleanliness_rating) AS cleanliness_rating, AVG(overall_rating) AS overall_rating \
-      FROM house, review_house \
-      WHERE house.id=review_house.house_id AND house.landlord_id=? \
-      GROUP BY house.id;',
-    	[req.query.id, req.query.id],
-    	function(err,query_ret){
-        	if(err) winston.log('error',err);
-        	if(query_ret.length != 2){
-        		winston.log('error', 'incorrect number of rows returned by query');
-        		next(err);
-        	}else{
-        		page_args = query_ret[0][0];
-            page_args.houses = query_ret[1];
-      			res.render('landlord', page_args);
-      		}
-    	}
-  	);
+router.get('/landlord', function(req, res, next) { 
+  Landlord.findOne({ '_id': req.query.id }, function(err, landlord){
+    if(err){
+      winston.log('error',err);
+      next(err);
+    }
+    if(landlord === null){
+      next();
+    }
+    House.find({ 'landlord_id': req.query.id }, function(err, owned_houses){
+      if(err){
+        winston.log('error',err);
+        next(err);
+      }
+      page_args = {};
+      page_args.landlord = landlord;
+      page_args.houses = owned_houses;
+      res.render('landlord', page_args);
+    });
+  });
 });
 
 module.exports = router;
