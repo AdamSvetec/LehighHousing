cbe04dc6d089139864abbb553c2bbb0d709a1d2f
Adam Svetec <adamsvetec@gmail.com>
2017-03-05 13:02:19 -0500

    add avg rating field and automatic populator for that field when document is loaded

--- models/house.js
+++ models/house.js
@@ -1,32 +1,51 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var houseReviewSchema = new Schema({
+  email: String,
+  message: String,
+  date: Date,
+  year_rented: String,
+  room_size_rating: Number,
+  cleanliness_rating: Number,
+  overall_rating: Number,
+  user_confirmed: Boolean,
+  system_confirmed: Boolean
+});
+
 var houseSchema = new Schema({
   _id: Schema.Types.ObjectId,
   address: String,
   lat: Number,
   lng: Number,
-  landlord_id: Schema.Types.ObjectId,
+  landlord_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Landlord'},
   bedroom_cnt: Number,
   bathroom_cnt: Number,
   parking_spot_cnt: Number,
+  avg_room_size_rating: Number,
+  avg_cleanliness_rating: Number,
+  avg_overall_rating: Number,
   availability: [ {
     year: String,
     available: Boolean,
     rent: Number 
   } ],
-  reviews: [ {
-    email: String,
-    message: String,
-    date: Date,
-    year_rented: String,
-    room_size_rating: Number,
-    cleanliness_rating: Number,
-    overall_rating: Number,
-    user_confirmed: Boolean,
-    system_confirmed: Boolean
-  } ],
+  reviews: [ houseReviewSchema ],
   attributes: [ { name: String } ]
+});
+
+/* Update rating averages
+  -Would prob be better to cache this? */
+houseSchema.post('init', function (next) {
+  var room_size_total=0, cleanliness_total=0, overall_total=0; 
+  this.reviews.forEach(function (review) { 
+    room_size_total+=review.room_size_rating; 
+    cleanliness_total+=review.cleanliness_rating; 
+    overall_total+=review.overall_rating; 
+  });
+  this.avg_room_size_rating = this.reviews.length > 0 ? room_size_total / this.reviews.length : 0;
+  this.avg_cleanliness_rating = this.reviews.length > 0 ? cleanliness_total / this.reviews.length : 0;
+  this.avg_overall_rating = this.reviews.length > 0 ? overall_total / this.reviews.length : 0;
 });
 
 var house = mongoose.model('House', houseSchema, 'house');
--- models/landlord.js
+++ models/landlord.js
@@ -1,23 +1,42 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var landlordReviewSchema = new Schema({
+  email: String,
+    message: String,
+    date: Date,
+    year_rented: String,
+    leniency_rating: Number,
+    fairness_rating: Number,
+    repair_rating: Number,
+    user_confirmed: Boolean,
+    system_confirmed: Boolean
+});
+
 var landlordSchema = new Schema({
   _id: Schema.Types.ObjectId,
   name: String,
   phone_num: String,
   email: String,
   website: String,
-  reviews: [ {
-  	email: String,
-  	message: String,
-  	date: Date,
-  	year_rented: String,
-  	leniency_rating: Number,
-  	fairness_rating: Number,
-  	repair_rating: Number,
-  	user_confirmed: Boolean,
-  	system_confirmed: Boolean
-  } ]
+  avg_leniency_rating: Number,
+  avg_fairness_rating: Number,
+  avg_repair_rating: Number,
+  reviews: [ landlordReviewSchema ]
+});
+
+/* Update rating averages
+  -Would prob be better to cache this? */
+landlordSchema.post('init', function (next) {
+  var leniency_total=0, fairness_total=0, repair_total=0; 
+  this.reviews.forEach(function (review) { 
+    leniency_total+=review.leniency_rating; 
+    fairness_total+=review.fairness_rating; 
+    repair_total+=review.repair_rating; 
+  });
+  this.avg_leniency_rating = this.reviews.length > 0 ? leniency_total / this.reviews.length : 0;
+  this.avg_fairness_rating = this.reviews.length > 0 ? fairness_total / this.reviews.length : 0;
+  this.avg_repair_rating = this.reviews.length > 0 ? repair_total / this.reviews.length : 0;
 });
 
 var landlord = mongoose.model('Landlord', landlordSchema, 'landlord');
